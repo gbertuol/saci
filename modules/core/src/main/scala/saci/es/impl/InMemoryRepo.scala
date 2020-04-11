94c10f5c75160a173cc648783bf57af486b47c3b
Guilherme Bertuol <guilherme.bertuol@gmail.com>
2020-04-11 13:25:01 +0100

    added in memory repo for testing

--- modules/core/src/main/scala/saci/es/impl/InMemoryRepo.scala
+++ modules/core/src/main/scala/saci/es/impl/InMemoryRepo.scala
@@ -1,5 +1,56 @@
 package saci.es.impl
 
-package saci.es.impl
+import java.time.Instant
 
-final case class InMemoryRepo() extends Re
+object InMemoryRepo {
+  import saci.data._
+  import scala.collection.mutable
+  import io.circe.Json
+  import saci.data.WriteResult
+  import cats.effect.Sync
+  import cats.effect.concurrent.Ref
+  import cats.implicits._
+
+  def apply[F[_]: Sync]: F[Repository[F]] = {
+    for {
+      db            <- Ref[F].of(mutable.Map.empty[Key, mutable.Buffer[Node]])
+      globalCounter <- Ref[F].of(0L)
+    } yield new Repository[F] {
+      override def query(agType: AggregateType, agId: AggregateId, from: Version): fs2.Stream[F, RecordedEvent] = {
+        for {
+          _db <- fs2.Stream.eval(db.get)
+          aggregate = _db.getOrElse(Key(agType, agId), mutable.Buffer.empty[Node])
+          data = aggregate.dropWhile(_.version < from).map(d => RecordedEvent(d.evId, agType, agId, d.version, d.data, d.created))
+          events <- fs2.Stream.fromIterator(data.iterator)
+        } yield events
+      }
+
+      override def put(evId: EventId, agType: AggregateType, agId: AggregateId, version: Version, data: Json): F[WriteResult] = {
+        for {
+          _db       <- db.get
+          timestamp <- Sync[F].delay(Instant.now)
+          _ <- Sync[F].catchNonFatal {
+            val key = Key(agType, agId)
+            val aggregate = _db.getOrElseUpdate(key, mutable.Buffer.empty[Node])
+            if (aggregate.isEmpty) {
+              if (version != 1) {
+                throw OptimisticConcurrencyCheckError(s"empty stream of $key requires first version to be 1, got $version instead")
+              }
+              aggregate.addOne(Node(version, evId, timestamp, data))
+            } else {
+              val lastNode = aggregate.last
+              if (lastNode.version != (version - 1)) {
+                throw OptimisticConcurrencyCheckError(s"outdated version $version of $key current is ${lastNode.version}")
+              }
+              aggregate.addOne(Node(version, evId, timestamp, data))
+            }
+          }
+          sqNr <- globalCounter.modify(x => (x + 1, x))
+        } yield WriteResult(evId, agType, sqNr)
+      }
+    }
+  }
+
+  final case class Key(agType: AggregateType, agId: AggregateId)
+  final case class Node(version: Version, evId: EventId, created: Instant, data: Json)
+}
--- modules/core/src/test/scala/saci/es/GetAggregateSpec.scala
+++ modules/core/src/test/scala/saci/es/GetAggregateSpec.scala
@@ -1,42 +1,32 @@
 package saci.es
 
-import saci.data._
 import saci.es.impl._
 import org.specs2.mutable.Specification
 import cats.effect.testing.specs2.CatsEffect
 import cats.effect.IO
 import io.circe.syntax._
 import java.{util => ju}
-import java.time.Instant
-import io.circe.Json
 
 class GetAggregateSpec extends Specification with CatsEffect {
-
-  implicit val repo = new Repository[IO] {
-
-    override def put(evId: EventId, agType: AggregateType, agId: AggregateId, version: Version, data: Json): IO[WriteResult] = ???
-
-    override def query(sgType: AggregateType, agId: AggregateId, from: Version): fs2.Stream[IO, RecordedEvent] = {
-      fs2.Stream.emit(
-        RecordedEvent(
-          ju.UUID.randomUUID(),
-          sgType,
-          agId,
-          from,
-          """{"a": 1}""".asJson,
-          Instant.MIN
-        )
-      )
-    }
-  }
 
   "GetAggregate" should {
     "get the aggregate" in {
       for {
-        get <- GetAggregate.apply[IO].get("foo", "bar", 1).compile.last
-        _   <- IO(get.map(_.version) === Some(1))
-        _   <- IO(get.map(_.data) === Some("""{"a": 1}""".asJson))
-      } yield success
+        repo <- InMemoryRepo.apply[IO]
+        evId <- IO.delay(ju.UUID.randomUUID())
+        agType = "agType"
+        agId = "agId"
+        version = 1
+        data = """{}""".asJson
+        _ <- repo.put(evId, agType, agId, version, data)
+      } yield {
+        implicit val _repo = repo
+        for {
+          get <- GetAggregate.apply[IO].get(agType, agId, 1).compile.last
+          _   <- IO(get.map(_.version) === Some(1))
+          _   <- IO(get.map(_.data) === Some("""{}""".asJson))
+        } yield success
+      }
     }
   }
 }
